f9d067162820e66b60b52f7943e72dfc76b9211c
robert <robert11176989@gmail.com>
2020-08-30 17:23:46 +0800

    learning JSX 转换

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-import {createElement,Component,render} from "./toy-react";
 
-// for (let i of [1,2,3,4]) {
-//     console.log(i);
-// }
-
-class Mycomponent extends Component{
-
-    render(){
-        return <div>
-                <h1>my component</h1>
-                {this.children}
-            </div>
+function createElement(tagName, attributes, ...children){
+    let e = document.createElement(tagName);
+    for(let p in attributes) {
+        e.setAttribute(p, attributes[p]);
     }
+    for(let child of children) {
+        if (typeof child === "string"){
+            child = document.createTextNode(children);
+        }
+        e.appendChild(child);
+    }
+    return e;
 }
-
-
 
 // window.a  = <div id='a' class="c">
 //      <div>abc</div>
@@ -22,15 +19,32 @@ class Mycomponent extends Component{
 //      <div></div>
 //     </div>
 
-// document.body.appendChild ( 
-//    <div id='a' class="c">
-//      <div>abc</div>
-//      <div><div></div></div>
-//      <div></div>
-//     </div>);
-
-render(<Mycomponent id='a' class="c">
+document.body.appendChild ( 
+   <div id='a' class="c">
      <div>abc</div>
+     <div>
+         <div></div>
+    </div>
      <div></div>
-     <div></div>
-    </Mycomponent>, document.body);
+    </div>);
+
+/**
+ * 上面这段标签会被插件翻译成如下
+ * 可以自己运行 npx webpack 试试
+ * 子元素会被当成第3个参数传进去；多个并列的子元素，则会作为第3个、第4个...参数传进去
+ */
+
+/** 
+document.body.appendChild(
+    createElement("div",
+        {             //属性当做一个对象，作为第二个参数传进去
+            id: "a",
+            "class": "c"
+        },
+        createElement("div", null, "abc"),
+        createElement("div", null, createElement("div", null)), //子元素也有子元素的话，也是一样，其实就是递归了
+        createElement("div", null))
+);
+
+*/
+
--- toy-react.js
+++ toy-react.js
@@ -1,84 +1,84 @@
-class ElementWrapper {
-    constructor(type){
-        this.root = document.createElement(type);
-    }
-    setAttribute(name,value){
-        this.root.setAttribute(name, value);
-    }
-    appendChild(component){
-        this.root.appendChild(component.root);
-    }
-}
+// class ElementWrapper {
+//     constructor(type){
+//         this.root = document.createElement(type);
+//     }
+//     setAttribute(name,value){
+//         this.root.setAttribute(name, value);
+//     }
+//     appendChild(component){
+//         this.root.appendChild(component.root);
+//     }
+// }
 
-class TextWrapper {
-    constructor(content){
-        this.root = document.createTextNode(content);
-    }
+// class TextWrapper {
+//     constructor(content){
+//         this.root = document.createTextNode(content);
+//     }
 
-}
+// }
 
-export class Component {
-    constructor(){
-        this.props = Object.create(null);
-        this.children = [];
-        this._root = null;
-    }
-    setAttribute(name,value){
-        this.props[name] = value;
-    }
-    appendChild(component){
-        this.children.push(component);
-    }
+// export class Component {
+//     constructor(){
+//         this.props = Object.create(null);
+//         this.children = [];
+//         this._root = null;
+//     }
+//     setAttribute(name,value){
+//         this.props[name] = value;
+//     }
+//     appendChild(component){
+//         this.children.push(component);
+//     }
 
-    // es6的写法，会产生一个getter
-    // 视频说，get root 是一个真实的render
-    get root(){
-        if (!this._root) {
-            // 不太懂，待理解
-            // 如果render出的还是component，就会发递归，一直到它变成ElementWrapper或TextWrapper，有根节点的这样一个component
-            this._root = this.render().root;
-        }
-        return this._root;
-    }
-}
+//     // es6的写法，会产生一个getter
+//     // 视频说，get root 是一个真实的render
+//     get root(){
+//         if (!this._root) {
+//             // 不太懂，待理解
+//             // 如果render出的还是component，就会发递归，一直到它变成ElementWrapper或TextWrapper，有根节点的这样一个component
+//             this._root = this.render().root;
+//         }
+//         return this._root;
+//     }
+// }
 
-/**
- * 
- * @param {*} type 
- * @param {*} attributes 属性列表，是一个object,例如{ id: "a", "class": "c"}
- * @param  {...any} children 子节点
- */
-export function createElement(type, attributes, ...children){
-    let  e;
-    if (typeof type === "string"){
-        // e = document.createElement(type);
-        e = new ElementWrapper(type);
-    }else{
-        e = new type;
-    }
+// /**
+//  * 
+//  * @param {*} type 
+//  * @param {*} attributes 属性列表，是一个object,例如{ id: "a", "class": "c"}
+//  * @param  {...any} children 子节点
+//  */
+// export function createElement(type, attributes, ...children){
+//     let  e;
+//     if (typeof type === "string"){
+//         // e = document.createElement(type);
+//         e = new ElementWrapper(type);
+//     }else{
+//         e = new type;
+//     }
 
-    for (let p in attributes) {
-        e.setAttribute(p, attributes[p]);
-    }
+//     for (let p in attributes) {
+//         e.setAttribute(p, attributes[p]);
+//     }
 
-    let insertChildren = (children) => {
+//     let insertChildren = (children) => {
 
-        for (let child of children) {
-            if (typeof child === "string") {
-                // child = document.createTextNode(child);
-                child = new TextWrapper(child);
-            }
-            if (typeof child === 'object' && child instanceof Array) {
-                insertChildren(child);
-            }else {
-                e.appendChild(child);
-            }
-        }
-    }
-    insertChildren(children);
-    return e;
-}
+//         for (let child of children) {
+//             if (typeof child === "string") {
+//                 // child = document.createTextNode(child);
+//                 child = new TextWrapper(child);
+//             }
+//             if (typeof child === 'object' && child instanceof Array) {
+//                 insertChildren(child);
+//             }else {
+//                 e.appendChild(child);
+//             }
+//         }
+//     }
+//     insertChildren(children);
+//     return e;
+// }
 
-export function render(component, parentElement) {
-    parentElement.appendChild(component.root);
-}
+// export function render(component, parentElement) {
+//     parentElement.appendChild(component.root);
+// }
